31d1aea5632c94ae656c0fdbb20b51b3f665f56a
takkanm <takkanm@gmail.com>
2015-05-02 17:53:43 +0900

    add example code

---
+++ examples/tcp_test.rs
@@ -0,0 +1,18 @@
+extern crate rust_fluent;
+use rust_fluent::tcp;
+
+use std::collections::HashMap;
+
+
+fn main() {
+    let mut obj: HashMap<String, String> = HashMap::new();
+    obj.insert("key".to_string(), "value".to_string());
+
+    let fluentd = tcp::Fluentd {
+      address: "0.0.0.0".to_string(),
+      port: 24224,
+      tag: "foo".to_string(),
+    };
+
+    fluentd.write(obj);
+}
