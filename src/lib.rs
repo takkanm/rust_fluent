8a3d8c0572333053f936eab6a0515850d324e90d
takkanm <takkanm@gmail.com>
2015-05-02 17:21:01 +0900

    init tcp mod

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+pub mod tcp;
+
+#[cfg(test)]
+mod test {
+    use tcp;
+    use std::collections::HashMap;
+
+    #[test]
+    fn tcp_write() {
+        let mut object: HashMap<String, String> = HashMap::new();
+        object.insert("key".to_string(), "value".to_string());
+
+        let fluentd = tcp::Fulentd {
+            address: "0.0.0.0".to_string(),
+            port: 24224,
+            tag: "foo".to_string(),
+        };
+
+        fluentd.write(object);
+    }
+}
---
+++ src/tcp.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+extern crate rustc_serialize;
+use self::rustc_serialize::json;
+
+extern crate time;
+
+pub struct Fulentd {
+    pub address: String,
+    pub port: u16,
+    pub tag: String,
+}
+
+impl Fulentd {
+    pub fn write(&self, object: HashMap<String, String>) {
+        let tag = json::encode(&self.tag).unwrap();
+        let now = time::now();
+        let record = json::encode(&object).unwrap();
+        let message = format!("[{},{},{}]", tag, now.to_timespec().sec, record);
+
+        let endpoint: &str = self.address.as_ref();
+        let mut stream = TcpStream::connect((endpoint, self.port)).unwrap();
+
+        let _ = stream.write(&message.into_bytes());
+
+        drop(stream);
+    }
+}
