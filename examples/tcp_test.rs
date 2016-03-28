0c0b132d5cca93195ab00ddcd54f2783f89e5047
Hiroshi Hatake <hatake@clear-code.com>
2016-03-28 16:09:42 +0900

    tcp: Provide ::new()

--- examples/tcp_test.rs
+++ examples/tcp_test.rs
@@ -8,10 +8,7 @@ fn main() {
     let mut obj: HashMap<String, String> = HashMap::new();
     obj.insert("key".to_string(), "value".to_string());
 
-    let fluentd = tcp::Fluentd {
-      address: "0.0.0.0:24224",
-      tag: "foo",
-    };
+    let fluentd = tcp::Fluentd::new("0.0.0.0:24224","foo");
 
     let _ = fluentd.write(&obj);
 }
