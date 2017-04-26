3011d70cc71fc304344626a841790a60deab57bc
takkanm <takkanm@gmail.com>
2017-04-27 07:56:50 +0900

    remove blank line

--- examples/tcp_test.rs
+++ examples/tcp_test.rs
@@ -3,7 +3,6 @@ use rust_fluent::tcp;
 
 use std::collections::HashMap;
 
-
 fn main() {
     let mut obj: HashMap<String, String> = HashMap::new();
     obj.insert("key".to_string(), "value".to_string());
